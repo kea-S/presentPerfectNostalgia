94d47b7614d27447a424ff22606939a7e726acbe
kea-S <keaharvan1@gmail.com>
2025-02-19 23:00:11 +0800

    Add naive preload

--- circuits/src/components/Modals.tsx
+++ circuits/src/components/Modals.tsx
@@ -8,16 +8,15 @@ interface ModalProps {
 function Modal({ isOpen, children }: ModalProps) {
 
     return (
-        <>
-            {isOpen && 
-                <div style={{pointerEvents: "none"}}
-                    className="fixed z-20 top-1/2 left-1/2
-                    transform -translate-x-1/2 -translate-y-1/2 
-                    bg-black text-white w-192 h-176 border 
-                    [&_*]:!border-white !border-white p-4 hidden sm:block">
-                    {children}
-                </div>}
-        </>
+        <div className={isOpen ? 'block' : 'hidden'}>
+            <div style={{pointerEvents: "none"}}
+                className="fixed z-20 top-1/2 left-1/2
+                transform -translate-x-1/2 -translate-y-1/2 
+                bg-black text-white w-192 h-176 border 
+                [&_*]:!border-white !border-white p-4 hidden sm:block">
+                {children}
+            </div>
+        </div>
     );
 }
 
