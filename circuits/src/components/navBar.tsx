05f8f0d3f1de1b0299999079b48d0988088032d6
kea-S <keaharvan1@gmail.com>
2025-02-20 01:12:12 +0800

    Fix phone navbar size

--- circuits/src/components/navBar.tsx
+++ circuits/src/components/navBar.tsx
@@ -1,29 +1,29 @@
 function NavBar() {
 
     return (
-        <div className="box-content w-32 md:w-96 xl:w-150 h-60">
-            <div className="h-40 flex">
+        <div className="box-content w-36 sm:w-60 md:w-96 xl:w-150 h-60">
+            <div className="h-20 md:h-40 flex">
                 <div className="flex basis-2/3 p-3">
                     <a className="border flex-1 flex justify-center items-center font-normal no-underline text-current
-                        hover:no-underline hover:text-current bg-black"
+                        hover:no-underline hover:text-current bg-black p-1"
                         href="/KeaHarvanSuyanto_Resume.pdf"
                         download>
                         <h6 className="text-xs md:text-base">Resume</h6>
                     </a>
                 </div>
-                <div className="flex h-32 basis-1/3 self-end p-3">
+                <div className="flex h-16 md:h-32 basis-1/3 self-end p-3">
                     <a className="border flex-1 flex justify-center items-center font-normal no-underline text-current
-                        hover:no-underline hover:text-current bg-black"
+                        hover:no-underline hover:text-current bg-black p-1"
                         href="https://github.com/kea-S"
                         target="_blank">
                         <h6 className="text-xs md:text-base">Github</h6>
                     </a>
                 </div>
             </div>
-            <div className="h-20 flex justify-around">
+            <div className="h-14 md:h-20 flex justify-around">
                 <div className="flex basis-2/5 p-3">
                     <a className="border flex-1 flex justify-center items-center font-normal no-underline text-current
-                        hover:no-underline hover:text-current bg-black"
+                        hover:no-underline hover:text-current bg-black p-1"
                         href="mailto:kea_harvan@u.nus.edu"
                         target="_blank">
                         <h6 className="text-xs md:text-base">Email</h6>
@@ -31,7 +31,7 @@ function NavBar() {
                 </div>
                 <div className="flex basis-2/5 p-3">
                     <a className="border flex-1 flex justify-center items-center font-normal no-underline text-current
-                        hover:no-underline hover:text-current bg-black"
+                        hover:no-underline hover:text-current bg-black p-1"
                         href="https://www.linkedin.com/in/keaharvan"
                         target="_blank">
                         <h6 className="text-xs md:text-base">LinkedIn</h6>
